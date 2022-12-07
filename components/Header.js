7410ac3071cf893acf39d147094169016c87a19f
Varun Joshi <vj903540@gmail.com>
2022-12-07 16:23:00 +0530

    all done

---
+++ components/Header.js
@@ -0,0 +1,85 @@
+import Image from "next/image";
+import SearchRoundedIcon from "@mui/icons-material/SearchRounded";
+import HeaderLink from "./HeaderLink";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import HomeRoundedIcon from "@mui/icons-material/HomeRounded";
+import OndemandVideoSharpIcon from "@mui/icons-material/OndemandVideoSharp";
+import { Avatar } from "@mui/material";
+import { useEffect, useState } from "react";
+import { useTheme } from "next-themes";
+import { motion } from "framer-motion";
+
+const spring = {
+  type: "spring",
+  stiffness: 700,
+  damping: 30,
+};
+
+function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme, theme } = useTheme();
+
+  // After mounting, we have access to the theme
+  useEffect(() => setMounted(true), []);
+
+  console.log("Current theme is", theme);
+
+  return (
+    <header className="sticky top-0 z-40 bg-white dark:bg-[#1D2226] flex items-center justify-around py-1.5 px-3 focus-within:shadow-lg">
+      {/* Left */}
+      <div className="flex items-center space-x-2 w-full max-w-xs">
+        {mounted && (
+          <>
+            {resolvedTheme === "dark" ? (
+              <Image src="https://rb.gy/fcwqfm" width={60} height={60} />
+            ) : (
+              <Image src="https://rb.gy/fcwqfm" width={75} height={75} />
+            )}
+          </>
+        )}
+
+        <div className="flex items-center space-x-1 dark:md:bg-gray-700 py-2.5 px-4 rounded w-full">
+          <SearchRoundedIcon />
+          <input
+            type="text"
+            placeholder="Search"
+            className="hidden md:inline-flex bg-transparent text-sm focus:outline-none placeholder-black/70 dark:placeholder-white/75 flex-grow"
+          />
+        </div>
+      </div>
+      {/* Right */}
+
+      <div className="flex items-center space-x-6">
+        <HeaderLink Icon={HomeRoundedIcon} text="Home" feed active />
+        <HeaderLink Icon={ChatIcon} text="Messaging" feed />
+        <HeaderLink Icon={OndemandVideoSharpIcon} text="Learning" feed hidden/>
+        <HeaderLink Icon={NotificationsIcon} text="Notifications" feed hidden/>
+        <HeaderLink Icon={Avatar} text="Logout" feed avatar/>
+
+        {/* Dark mode toggle */}
+        {mounted && (
+          <div
+            className={`bg-gray-600 flex items-center px-0.5 rounded-full h-6 w-12 cursor-pointer flex-shrink-0 relative ${
+              resolvedTheme === "dark" ? "justify-end" : "justify-start"
+            }`}
+            onClick={() =>
+              setTheme(resolvedTheme === "dark" ? "light" : "dark")
+            }
+          >
+            <span className="absolute left-0">🌜</span>
+            <motion.div
+              className="w-5 h-5 bg-white rounded-full z-40"
+              layout
+              transition={spring}
+            />
+
+            <span className="absolute right-0.5">🌞</span>
+          </div>
+        )}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
